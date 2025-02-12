16c69a36b2ce730c06ada440be4ce113c0c4207d
PranavKadam10 <118792746+PranavKadam10@users.noreply.github.com>
2025-02-12 23:58:37 +0530

    react setup

---
+++ frontend/src/pages/Signin.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import '../style/Signin.css'
+
+const Signin = () => {
+  return (
+    <>
+        <div className='maindiv'>
+            <div className='box'>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Signin
