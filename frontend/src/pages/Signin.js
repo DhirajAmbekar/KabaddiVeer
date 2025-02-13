54dda265cad06fd043da9306ef06376d8b3a6b0e
PranavKadam10 <118792746+PranavKadam10@users.noreply.github.com>
2025-02-13 14:57:42 +0530

    signin page

--- frontend/src/pages/Signin.js
+++ frontend/src/pages/Signin.js
@@ -5,7 +5,17 @@ const Signin = () => {
   return (
     <>
         <div className='maindiv'>
+            <div>
+            <img src="frontend\src\asset\Netflix_Logo.jpg" alt='netflix-logoo'/>
+            </div>
             <div className='box'>
+                <div className='inner_box'>
+                   <h2>Sign In</h2>
+                   <input className='inputclas' type='email' placeholder='Email or mobile number'></input>
+                   <input className='inputclas' type='password' placeholder='Password'></input>
+                   <button className='submit_btn'>Sign in</button>
+                </div>
+                {/* <input className='input'></input> */}
             </div>
         </div>
     </>
